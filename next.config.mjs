f04ac08b8f3b13ec4c9c4d9feca307039c384bca
bishoe01 <bishoe01@gmail.com>
2024-08-07 11:47:50 +0900

    feat: enable styled-components in next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,9 @@ import withPWA from "next-pwa";
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   // reactStrictMode: true,
+  compiler: {
+    styledComponents: true,
+  },
 };
 
 export default withPWA({
