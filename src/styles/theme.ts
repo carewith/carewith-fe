16ab1f0cd116fcf14ff8d50dc098f636a47a99db
bishoe01 <bishoe01@gmail.com>
2024-08-08 15:26:09 +0900

    feat : 디자인 시스템 적용

---
+++ src/styles/theme.ts
@@ -0,0 +1,47 @@
+import { DefaultTheme } from "styled-components";
+const theme :DefaultTheme = {
+  colors: {
+    primary: {
+      blue01: "#3D49FF",
+      blue02: "#5A81FA",
+      blue03: "#CEDEFF",
+      blue04: "#E9F0FF",
+    },
+    navy: {
+      navy01: "#243144",
+      navy02: "#1B2337",
+    },
+    grey: {
+      grey01: "#454761",
+      grey02: "#808AAB",
+      grey03: "#CAD0E3",
+    },
+    text: {
+      text01: "#201F22",
+      text02: "#706F75",
+      text03: "#A4A3AB",
+      text04: "#D5D4DD",
+    },
+    alert: {
+      red: "#FF3737",
+      bold: "#E14143",
+      light01: "#FF7C7E",
+      light02: "#FFB8B9",
+    },
+    green: {
+      green: "#04C75B",
+      bold: "#0DAA5C",
+      light01: "#6EC796",
+      light02: "#B8E2CA",
+      backgreen: "#DFF0E6",
+    },
+    border: "#F4F5F9",
+    background: "#f5f5f5",
+  },
+   layout: {
+      maxWidth: "768px",
+      padding: "0 1rem",
+    }
+};
+
+export default theme;
