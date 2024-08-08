9e94840c27fbe82dff89a0d228ec70fd3c829bb3
bishoe01 <bishoe01@gmail.com>
2024-08-08 13:35:48 +0900

    feat: 모바일뷰를 위한 Container컴포넌트 구현

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import "../styles/globals.css";
+import Container from "@/components/layout/Container";
 export const metadata: Metadata = {
   title: "CAREWITH",
   description: "치매 환자 알약 디스펜서 관리 App",
@@ -13,9 +14,12 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="manifest" href="/manifest.json" />
       </head>
-      <body>{children}</body>
+      <body>
+        <Container>{children}</Container>
+      </body>
     </html>
   );
 }
